08c4197e6804a24d5b6641eae7de5da73fdc515e
SaitoYuki <saito@ChickenFilet-O.local>
2022-05-07 21:02:23 +0900

    first commit

---
+++ crew-training/crew_trainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  crew_trainingApp.swift
+//  crew-training
+//
+//  Created by 齋藤有希 on 2022/05/07.
+//
+
+import SwiftUI
+
+@main
+struct crew_trainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
